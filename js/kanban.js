3b09d8626d6062b707b6247160be6f39515642b8
RAJVIJAY <raju.rajvijay619@gmail.com>
2021-08-15 02:10:30 +0530

    create item div and add event listeners

--- js/kanban.js
+++ js/kanban.js
@@ -16,7 +16,46 @@ add_btn.addEventListener('click', () => {
 });
 
 const create_item = () => {
+  var item = document.createElement("div");
+
+  item.classList.add("item");
+  item.id = 'item-'+order;
+  item.draggable = true;
+
+  item.addEventListener('dragstart', (event)=>{
+    return event.dataTransfer.setData('text',event.target.id);
+  })
+
+  item.addEventListener("dragend", (event)=>{
+    return event.dataTransfer.clearData();
+  })
+
+  var input = document.createElement("input");
+
+  item.appendChild(input);
+
+  var save_btn = document.createElement("button");
+
+  save_btn.innerHTML = "Save";
+
+  save_btn.addEventListener("click", ()=>{
+    error.innerHTML = '';
+
+    if ( input.value != ''){
+      order += 1;
+      item.innerHTML = input.value;
+      adding = false;
+    }else{
+      error.innerHTML = message;
+    }
+  });
+
+  item.appendChild(save_btn);
+
+  return item;
+
 };
 
 document.querySelectorAll('.drop').forEach(element => {
+  
 });
